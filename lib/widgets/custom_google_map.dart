f2ad1d4ef4712a9276dc98bc6344f03a2eb04011
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-11-03 21:12:11 +0200

    "Updated CustomGoogleMap widget: changed initial zoom level, added GoogleMapController, and added a button to reset camera position."

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -13,14 +13,47 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
-      zoom: 19,
+      zoom: 18,
       target: LatLng(29.978992404450963, 31.25088978734445),
     );
     super.initState();
   }
 
+  late GoogleMapController googleMapController;
+  @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(initialCameraPosition: initialCameraPosition);
+    return Stack(children: [
+      GoogleMap(
+        onMapCreated: (controller) {
+          googleMapController = controller;
+        },
+        initialCameraPosition: initialCameraPosition,
+        // cameraTargetBounds: CameraTargetBounds(
+        //   LatLngBounds(
+        //     southwest: const LatLng(29.977119405274014, 31.249217375163916),
+        //     northeast: const LatLng(29.98138624646672, 31.256507255830215),
+        //   ),
+        // ),
+      ),
+      Positioned(
+          bottom: 16,
+          left: 16,
+          right: 16,
+          child: ElevatedButton(
+              onPressed: () {
+                initialCameraPosition = const CameraPosition(
+                  zoom: 18,
+                  target: LatLng(29.978992404450963, 31.25088978734445),
+                );
+                setState(() {});
+              },
+              child: const Text('Change Location')))
+    ]);
   }
 }
