fc6257399a3c3479047723dfd6512599b56a00c7
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-11-03 20:40:33 +0200

    initialize googlemap to the app and create initial camera position

---
+++ lib/widgets/custom_google_map.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CustomGoogleMap extends StatefulWidget {
+  const CustomGoogleMap({super.key});
+
+  @override
+  State<CustomGoogleMap> createState() => _CustomGoogleMapState();
+}
+
+class _CustomGoogleMapState extends State<CustomGoogleMap> {
+  late CameraPosition initialCameraPosition;
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(
+      target: LatLng(29.978992404450963, 31.25088978734445),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(initialCameraPosition: initialCameraPosition);
+  }
+}
